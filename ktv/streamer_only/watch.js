b9acae435168337ddddc650c0e036e5dd68246a8
koganpa <119843620+koganpa@users.noreply.github.com>
2025-11-23 01:08:42 +0900

    Update watch.js

--- ktv/streamer_only/watch.js
+++ ktv/streamer_only/watch.js
@@ -1,4 +1,7 @@
-    const remoteArea = document.getElementById('remote-area');
+let remote_audio = false;
+let remote_video = false;
+
+const remoteArea = document.getElementById('remote-area');
 
 
     const roomIdInput = document.getElementById('room-id');
@@ -19,10 +22,41 @@
 
 
         const subscribeAndAttach = async (publication) => {
+    if (publication.publisher.id === me.id) return;
+    const { stream } = await me.subscribe(publication.id);
 
-            ;
+    let newMedia;
+    switch (stream.track.kind) {
+        case 'video':
+            if (remote_video) {
+                const oldMedia = document.getElementById('remote-video');
+                oldMedia.parentNode.removeChild(oldMedia);
+            }
+            newMedia = document.createElement('video');
+            newMedia.id = "remote-video";
+            newMedia.playsInline = true;
+            newMedia.autoplay = true;
+            newMedia.controls = true;
+            remote_video = true;
+            break;
+        case 'audio':
+            if (remote_audio) {
+                const oldMedia = document.getElementById('remote-audio');
+                oldMedia.parentNode.removeChild(oldMedia);
+            }
+            newMedia = document.createElement('audio');
+            newMedia.id = "remote-audio";
+            newMedia.controls = true;
+            newMedia.autoplay = true;
+            remote_audio = true;
+            break;
+        default:
+            return;
+    }
+    stream.attach(newMedia);
+    remoteArea.appendChild(newMedia);
+}
 
-        }
 
 
 
