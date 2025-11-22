9c5a884fa425b31c13802ab8a0ddc92fd91d29b4
koganpa <119843620+koganpa@users.noreply.github.com>
2025-11-23 00:48:37 +0900

    Create stream.js

---
+++ ktv/streamer_only/stream.js
@@ -0,0 +1,46 @@
+const { SkyWayContext, SkyWayRoom, SkyWayStreamFactory, SkyWayAuthToken, uuidV4, nowInSec } = skyway_room;
+
+const token = new SkyWayAuthToken({
+  jti: uuidV4(),
+  iat: nowInSec(),
+  exp: nowInSec() + 60 * 60 * 24,
+  scope: {
+    app: {
+      id: '7b2695fe-e087-45b1-98b7-b4020baeca31',
+      turn: true,
+      actions: ['read'],
+      channels: [
+        {
+          id: '*',
+          name: '*',
+          actions: ['write'],
+          members: [
+            {
+              id: '*',
+              name: '*',
+              actions: ['write'],
+              publication: {
+                actions: ['write'],
+              },
+              subscription: {
+                actions: ['write'],
+              },
+            },
+          ],
+          sfuBots: [
+            {
+              actions: ['write'],
+              forwardings: [
+                {
+                  actions: ['write'],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+}).encode('SecretKEY');
+
+console.log(token);
